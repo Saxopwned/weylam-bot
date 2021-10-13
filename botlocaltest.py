f5b9ab6119d535b6c76cbc10d08f80a8de30b79c
Saxopwned <92110462+Saxopwned@users.noreply.github.com>
2021-10-13 19:12:44 -0400

    1

--- botlocaltest.py
+++ botlocaltest.py
@@ -2,17 +2,32 @@ import requests
 import bs4
 
 def main():
+    
+    poedbURL = str("https://poedb.tw/us/")
+    infoReqInput = input("Please enter a Path of Exile information request ") 
+    infoReqRaw = (infoReqInput.split(" ", 1))
+    infoReq = str(infoReqRaw[1])
+    infoReqTypeRaw = str(infoReqRaw[0])
 
-    poeInfoReq = input("Please enter a Path of Exile information request ") # !geminfo pulls gem information from poedb
-    print(poeInfoReq)
+    if infoReqTypeRaw.startswith("!"):
+        infoReqType = infoReqTypeRaw.strip("!")
+    else:
+        print("I'm sorry, that is not a valid input. Please begin the response with !")
+    
+    if " " in infoReq:
+        info_Req = infoReq.replace(" ", "_")
+    else:
+        info_Req = infoReq
 
-    def geminfo():
-        gemName = (poeInfoReq.split().pop(1))
-        print("Alright, here's some information about ", str(gemName))
+    poedbReqURL = (poedbURL + info_Req)
 
+    if infoReqType == "gem" or "unique" or "mod":
+        print("Searching PoE.DB for", info_Req)
+    else:
+        print("Could not identify the object type of your request")
 
-    if poeInfoReq.startswith("!geminfo"):
-        geminfo;
+    print(poedbReqURL)
 
+    
 if __name__ == "__main__":
-    main()
+    main();
